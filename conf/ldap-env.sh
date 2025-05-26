fe3ca463fbcbdfd29becb3ba01407ad7792f8725
man2601010 <man2601010@126.com>
2025-05-26 15:25:03 +0800

    Delete conf directory

--- conf/ldap-env.sh
+++
@@ -1,8 +0,0 @@
-# Enviroment variables can be set here,it will be sourced
-# during security service startup
-
-export JAVA_HOME=/home/work/software/java
-export SEC_HOME=/home/work/software/security
-export LOG_HOME=/home/work/security/logs
-export PID_HOME=/home/work/security/pid
-export SERVER_PORT=10010
